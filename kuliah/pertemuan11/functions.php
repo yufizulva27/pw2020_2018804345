54482d5e2f3704738e1fbb3d7f04a3638f8a80e6
yufi <yufi.gitlab@gmail.com>
2020-06-02 21:41:23 +0700

    menambhakan fitur edit & delete

---
+++ kuliah/pertemuan11/functions.php
@@ -0,0 +1,75 @@
+<?php 
+function koneksi() {
+  return mysqli_connect('localhost', 'root', '', 'pw2020_2018804345');
+}
+
+function query($query) {
+  $conn = koneksi();
+
+  $result = mysqli_query($conn, $query);
+
+  // jika hasilnya cuma 1
+  if(mysqli_num_rows($result) == 1) {
+    return mysqli_fetch_assoc($result);
+  }
+
+  $rows = [];
+  while($row = mysqli_fetch_assoc($result)) {
+    $rows[] = $row;
+}
+
+  return $rows;
+}
+
+function tambah($data) {
+  // var_dump($data)
+  $conn = koneksi();
+
+  // query insert data
+  $npm = htmlspecialchars($data['npm']);
+  $nama = htmlspecialchars($data['nama']);
+  $email = htmlspecialchars($data['email']);
+  $jurusan = htmlspecialchars($data['jurusan']);
+  $img = htmlspecialchars($data['gambar']);
+
+  $query = ("INSERT INTO mahasiswa VALUES('', '$nama', '$npm', '$email', '$jurusan', '$img')");
+
+  mysqli_query($conn, $query) or die(mysqli_error($conn));
+  return mysqli_affected_rows($conn);
+
+}
+
+function hapus($id) {
+  $conn = koneksi();
+
+  // query delete data
+  mysqli_query($conn, "DELETE FROM mahasiswa WHERE id = $id") or die(mysqli_error($conn));
+
+  return mysqli_affected_rows($conn);
+}
+
+function update($data) {
+  // var_dump($data)
+  $conn = koneksi();
+
+  // query insert data
+  $id = $data['id'];
+  $npm = htmlspecialchars($data['npm']);
+  $nama = htmlspecialchars($data['nama']);
+  $email = htmlspecialchars($data['email']);
+  $jurusan = htmlspecialchars($data['jurusan']);
+  $img = htmlspecialchars($data['gambar']);
+
+  $query = "UPDATE mahasiswa SET
+              nama = '$nama',
+              npm = '$npm',
+              email = '$email',
+              jurusan = '$jurusan',
+              gambar = '$img'
+              WHERE id = '$id' ";
+var_dump($query);
+  mysqli_query($conn, $query) or die(mysqli_error($conn));
+  return mysqli_affected_rows($conn);
+
+}
+?>
---
+++ kuliah/pertemuan11/tambah.php
@@ -0,0 +1,77 @@
+<?php 
+require 'functions.php';
+
+// cek apakah tombol tambah sudah ditekan
+if (isset($_POST['tambah'])) {
+  // var_dump($_POST);
+  if (tambah($_POST) > 0 ) {
+    echo "
+      <script>
+        alert('Data Berhasil ditambahkan');
+        document.location.href = 'index.php';
+      </script>
+    ";
+  } else {
+    echo "
+      <script>
+        alert('Data Gagal ditambahkan');
+      </script>
+    ";
+  }
+}
+
+// cek apakah data berhasil di tambah
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tambah Data Baru</title>
+</head>
+<body>
+<h3>Tambah Data Baru</h3>
+<form action="" method="POST">
+  <ul>
+    <li>
+      <label for="npm">
+        NPM :
+        <input type="text" name="npm" id="npm" autofocus>
+      </label>
+    </li>
+    <li>
+      <label for="nama">
+        Nama :
+        <input type="text" name="nama" id="nama" required>
+      </label>
+    </li>
+    <li>
+      <label for="email">
+        Email :
+        <input type="text" name="email" id="email" required>
+      </label>
+    </li>
+    <li>
+      <label for="jurusan">
+        Jurusan :
+        <input type="text" name="jurusan" id="jurusan" required>
+      </label>
+    </li>
+    <li>
+      <label for="gambar">
+        Image :
+        <input type="text" name="gambar" id="gambar" required>
+      </label>
+        <li>
+          <button type="submit" name="tambah">Tambahkan</button>
+        </li>
+    </li>
+  </ul>
+
+</form>
+
+<a href="index.php">kembali</a>
+  
+</body>
+</html>
